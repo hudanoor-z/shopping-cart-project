1918adbc974a85c702dce691b53542a6e89f0f6b
Noorulhuda <noorulhuda3052@gmail.com>
2026-05-05 17:12:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-let box = document.getElementById("box");
-let cartList = document.getElementById("cartList");
-let countText = document.getElementById("count");
 
-let cart = [];
+let box=document.getElementById("box");
+let countText=document.getElementById("count");
+let cartList=document.getElementById("cartList");
+let totalText=document.getElementById("total");
+let cart=[];
 
-let products = [
-  {name:"Infinix", price: 50000},
-  {name:"Apple", price: 90000}
+  let products = [
+  { name: "Samsung Galaxy A15", price: 58000 },
+  { name: "Apple iPhone 13", price: 185000 },
+  { name: "Infinix Note 40", price: 52000 },
+  { name: "Dell Inspiron Laptop", price: 145000 },
+  { name: "Sony Headphones WH-CH520", price: 12500 }
 ];
 
-// show products
-for(let i = 0; i < products.length; i++)
-{
-  box.innerHTML += `
-  <div class="card">
-    <p>${products[i].name}</p>
-    <p>${products[i].price}</p>
 
-    <button onclick="addToCart('${products[i].name}')">
-      Add To Cart
-    </button>
-  </div>`;
-}
-
-// add function
-function addToCart(name)
-{
-  cart.push(name);
-  updateCart();
-  alert("added " + name);
-}
-
-// remove function
-function removeItem(index)
-{
-  cart.splice(index, 1);
-  updateCart();
-}
-
-function updateCart()
-{
-  countText.innerText = "Cart Items: " + cart.length;
-
-  cartList.innerHTML = "";
-
-  for(let i = 0; i < cart.length; i++)
+  for(let i=0; i<products.length;i++)
   {
-    cartList.innerHTML += `
+    box.innerHTML +=`
+    <div class="card">
+    <p> ${products[i].name}</p>
+    <p> ${products[i].price}</p>
+    <button onclick="addToCart('${products[i].name}', ${products[i].price})"> add To Cart 🛒
+    </button>
+    
+    </div>`;
+  }
+  function addToCart(name,price)
+  {
+    cart.push({name:name,price:price})
+    updateCart();
+    alert("Added: " + name + " - " + price);
+  }
+  function removeItem(index)
+  {
+     cart.splice(index,1);
+     updateCart();
+  }
+  function updateCart()
+  {
+  countText.innerText = "Cart Items:" + cart.length;
+  cartList.innerHTML="";
+  let total=0;
+  for (let i=0;i<cart.length;i++)
+  {
+    total+=cart[i].price;
+    cartList.innerHTML+=`
     <li>
-      ${cart[i]}
-      <button onclick="removeItem(${i})">❌ Remove</button>
+    ${cart[i].name} - ${cart[i].price}
+     <button onclick="removeItem(${i})">❌ Remove</button>
     </li>`;
   }
+  totalText.innerText="Total:" + total +  "💰";
 }
+  
+  
+
